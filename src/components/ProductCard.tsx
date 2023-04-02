7335a828643d86d3ebe4299ad6b9733d0c597884
cyf0rk <marijanbatarilo.dev@gmail.com>
2023-04-02 23:20:37 +0200

    added discount

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import './ProductCard.scss';
+import { Product } from './ProductsListing';
 
-type ProdcutCardType = {
-    title: string,
-    description: string,
-    image: string,
-    price: number,
-    currency: string,
-    ageRestriction: number,
-    color: string,
+type ProdcutCardType = Product & {
     buttonText: string
 }
 
-const ProductCard: React.FC<ProdcutCardType> = ({ title, description, image, price, currency, ageRestriction, color, buttonText }): JSX.Element => {
+const ProductCard: React.FC<ProdcutCardType> = ({ title, description, image, price, discount, currency, ageRestriction, color, buttonText }): JSX.Element => {
+    function calculateDiscount(price:number, discount: number) {
+        const discountedPrice = price - (price * (discount / 100));
+        return Math.round(discountedPrice * 100) / 100;
+    }
+
     return (
         <Card className="product-card">
             <div className="card-top-content">
@@ -25,7 +24,17 @@ const ProductCard: React.FC<ProdcutCardType> = ({ title, description, image, pri
                     <h2 style={{color: color}}>{title}</h2>
                     <p>{description}</p>
                 </div>
-                <div className="product-price" style={{color: color}}>{currency}{price}</div>
+                <div className="product-price" style={{color: color}}>
+                    {  discount ?
+                        <>
+                            <span className="product-price-new">{currency}{calculateDiscount(price, discount)}</span>
+                            <span className="product-price-discounted">{currency}{price}</span>
+                            <span className="product-discount-description">Limited {discount}% discount</span>
+                        </>
+                        :
+                        <span>{currency}{price}</span>
+                    }
+                </div>
                 <div className="card-content-cta">
                     <Button text={buttonText} color={color} onClick={() => {}} disabled={false} />
                     <div className="product-age-restriction" style={{backgroundColor: color}}>
